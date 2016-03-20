cb33bddc72010038b1c206c5c0b87b9d70a3c023
Roy Wei <roywjy@gmail.com>
2016-03-20 15:55:54 +0800

    create comments model, controller, and vire files

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  resources :messages
+
+  resources :messages do
+    resources :comments
+  end
+
   root 'messages#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
