b86ba7892cffa43aadaff4a25ba6448a44d4b08d
Roy Wei <roywjy@gmail.com>
2016-03-20 15:14:59 +0800

    edit and delete actions

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,5 @@
 class MessagesController < ApplicationController
-  before_action :find_params, only: [:show, :edit, :update, :destroy]
+  before_action :find_message, only: [:show, :edit, :update, :destroy]
 
   def index
     @messages = Message.all.order("created_at DESC")
@@ -22,13 +22,29 @@ class MessagesController < ApplicationController
   def show
   end
 
+  def edit
+  end
+
+  def update
+    if @message.update(message_params)
+      redirect_to message_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @message.destroy
+    redirect_to root_path
+  end
+
   private
 
   def message_params
     params.require(:message).permit(:title, :description)
   end
 
-  def find_params
+  def find_message
     @message = Message.find(params[:id])
   end
 
